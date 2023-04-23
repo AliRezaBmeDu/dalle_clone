14ec98bd7283cc8668b2ef1646ce5dac64262102
AliReza <ali.reza.buet@gmail.com>
2023-04-23 18:54:41 +0600

    buttons added in the createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,7 +13,13 @@ const CreatePost = () => {
     prompt:'',
     photo: ''
   })
-  const [generatingImg, setGeneratingImg] = useState(false)
+
+const generateImage = () => {
+
+}
+
+
+  const [generatingImg, setGeneratingImg] = useState(true)
   const [loading, setLoading] = useState(false)
 
   const handleSubmit = ()  => {
@@ -58,7 +64,65 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe= {handleSurpriseMe}
           />
+
+          {/* here AI generated image will be shown */}
+          <div className='relative bg-gray-50 border
+            border-gray-300 text-gray-900 text-sm rounded-1g 
+            focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64
+            flex justify-center items-center'>
+            {
+              form.photo ? (
+              <img 
+                src={form.photo}
+                alt={form.prompt}
+                className='w-full h-full object-contain'
+              />
+              ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className='w-9/12 h-9/12 obect-contain 
+                opacity-40'
+              />
+              )
+            }
+            {
+              generatingImg && (
+                <div className='absolute inset-0 z-0 flex
+                justify-center items-center 
+                bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                  <Loader />
+                </div>
+              )
+            }
+
+          </div>
         </div>
+        <div className='mt-5 flex gap-5'>
+            <button
+              type="button"
+              onClick={generateImage}
+              className='text-white bg-green-700 font-medium
+              rounded-md text-sm w-full sm:w-auto px-5 py-2.5
+              text-center'
+            >
+              {generatingImg ? 'Generating...' : 'Generate'}
+            </button>
+        </div>
+
+        <div className='mt-10'>
+            <p className='mt-2 text-[@666e75] text-[14px]'>Once you have created the Image you want,
+             you can share that with your community</p>
+             <button type="submit"
+              className='mt-3 text-white bg-[#6469ff] font-medium
+              rounded-md text-sm w-full sm:w-auto
+              px-5 py-2.5 text-center'>
+              {
+                loading ? "Sharing..." : "Share with the community"
+              }
+             </button>
+        </div>
+
       </form>
     </section>
   )
