b79ac70d8055771a5821a7a9635e7b89ea44e0c1
AliReza <ali.reza.buet@gmail.com>
2023-04-23 11:42:30 +0600

    tailwind setup and necessary components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import {logo} from './assets';
+import {Home, CreatePost} from './pages';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <header className='w-full flex 
+      justify-between items-center bg-white 
+      sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to="/">
+        <img src={logo} alt="logo" className='w-28 object-contain' />
+        </Link>
+      </header>
+    </BrowserRouter>
   )
 }
 
